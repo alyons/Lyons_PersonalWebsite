e91246b9ac108119043d10f23f6735b05a4534f7
Alexander Lyons <alexander.n.lyons@gmail.com>
2016-02-04 11:32:37 -0500

    Adding some App Data

--- scripts/app.js
+++ scripts/app.js
@@ -4,34 +4,33 @@
 
   app.config(function ($routeProvider) {
     $routeProvider
-    .when("/home", {
-      // template:"<h1>Home</h1>"
-      checkbox.checked = false;
-      templateUrl: "pages/home.html"
-      console.log("Home clicked: " + checkbox.checked);
-    })
-    .when("/projects", {
-      // template:"<h1>Projects</h1>"
-      checkbox.checked = false;
-      templateUrl: "pages/projects.html"
-      console.log("Projects clicked: " + checkbox.checked);
-    })
-    .when("/about", {
-      // template:"<h1>About</h1>"
-      checkbox.checked = false;
-      templateUrl: "pages/about.html"
-	cosole.log("About clicked: " + checkbox.checked);
-    })
-    .when("/contact", {
-      // template:"<h1>Contact</h1>"
-      checkbox.checked = false;
-      templateUrl: "pages/contact.html"
-	cosole.log("Contact clicked: " + checkbox.checked);
-    })
-    .otherwise({
-      redirectTo: "/home"
-      // template: "<h1>Not Found</h1>"
-    });
+      .when("/home", {
+        // template:"<h1>Home</h1>"
+        // checkbox.checked = false;
+        templateUrl: "pages/home.html";
+        console.log("Home clicked: " + checkbox.checked);
+      })
+      .when("/projects", {
+        // template:"<h1>Projects</h1>"
+        // checkbox.checked = false;
+        templateUrl: "pages/projects.html";
+        console.log("Projects clicked: " + checkbox.checked);
+      })
+      .when("/about", {
+        // template:"<h1>About</h1>"
+        // checkbox.checked = false;
+        templateUrl: "pages/about.html";
+  	    cosole.log("About clicked: " + checkbox.checked);
+      })
+      .when("/contact", {
+        // template:"<h1>Contact</h1>"
+        checkbox.checked = false;
+        templateUrl: "pages/contact.html";
+  	    cosole.log("Contact clicked: " + checkbox.checked);
+      })
+      .otherwise({
+        redirectTo: "/home"
+        // template: "<h1>Not Found</h1>"
+      });
   });
-
 })();
