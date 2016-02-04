92efe268424ca15457e1ed9665337e3f64fdc2f9
Alexander Lyons <alexander.n.lyons@gmail.com>
2016-02-04 11:18:48 -0500

    Trying out Javascript to do more hiding...

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,27 @@
 (function () {
   var app = angular.module("app", ['ngRoute']);
+  var checkbox = document.getElementById('navmenu-trigger')
 
   app.config(function ($routeProvider) {
     $routeProvider
     .when("/home", {
       // template:"<h1>Home</h1>"
+      checkbox.checked = false;
       templateUrl: "pages/home.html"
     })
     .when("/projects", {
       // template:"<h1>Projects</h1>"
+      checkbox.checked = false;
       templateUrl: "pages/projects.html"
     })
     .when("/about", {
       // template:"<h1>About</h1>"
+      checkbox.checked = false;
       templateUrl: "pages/about.html"
     })
     .when("/contact", {
       // template:"<h1>Contact</h1>"
+      checkbox.checked = false;
       templateUrl: "pages/contact.html"
     })
     .otherwise({
