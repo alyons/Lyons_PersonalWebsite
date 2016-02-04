41840558078298422b879632762ace2faebc6a98
Alexander Lyons <alexander.n.lyons@gmail.com>
2016-02-04 11:25:33 -0500

    Adding modules for node

--- scripts/app.js
+++ scripts/app.js
@@ -8,21 +8,25 @@
       // template:"<h1>Home</h1>"
       checkbox.checked = false;
       templateUrl: "pages/home.html"
+      console.log("Home clicked: " + checkbox.checked);
     })
     .when("/projects", {
       // template:"<h1>Projects</h1>"
       checkbox.checked = false;
       templateUrl: "pages/projects.html"
+      console.log("Projects clicked: " + checkbox.checked);
     })
     .when("/about", {
       // template:"<h1>About</h1>"
       checkbox.checked = false;
       templateUrl: "pages/about.html"
+	cosole.log("About clicked: " + checkbox.checked);
     })
     .when("/contact", {
       // template:"<h1>Contact</h1>"
       checkbox.checked = false;
       templateUrl: "pages/contact.html"
+	cosole.log("Contact clicked: " + checkbox.checked);
     })
     .otherwise({
       redirectTo: "/home"
